8e840a47425255b16152ed253a65b249b516cf8d
nichollascarter <nichollascarter@gmail.com>
2021-10-24 15:38:16 +0300

    fix pipeline

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,9 @@ export default function subjx(params) {
 }
 
 Object.defineProperty(subjx, 'createObservable', {
-    value: () => {
-        return new Observable();
-    }
+    value: () => (
+        new Observable()
+    )
 });
 
 Object.defineProperty(subjx, 'Subjx', {
